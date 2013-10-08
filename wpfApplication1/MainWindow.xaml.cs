f364624b8a2597ccf44c9a700675ecbea260a251
akurt5 <myredshore@live.com.au>
2013-10-09 10:03:30 +1100

    First commit

--- wpfApplication1/MainWindow.xaml.cs
+++ wpfApplication1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Xml;
 
 namespace WpfApplication1
 {
@@ -51,17 +52,98 @@ namespace WpfApplication1
         {
             MessageBox.Show("Good Press");
         }
+        private Point mousepos;
+        private Image draggedimage;
 
-        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void canvas1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            CaptureMouse();
-            Point mouse =e.GetPosition(image1);
-            image1.RenderTransform = mouse;
+            var image = e.Source as Image;
+            if (image != null && canvas1.CaptureMouse())
+            {
+                Point mousepos = e.GetPosition(canvas1);
+                draggedimage = image;
+                Console.WriteLine("ButtonDown");
+            }
         }
 
-        private void image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void canvas1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            if (draggedimage != null)
+            {
+                canvas1.ReleaseMouseCapture();
+                draggedimage = null;
+                Console.WriteLine("ButtonUp");
+            }
+        }
 
+        private void canvas1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedimage != null)
+            {
+                var offset =  e.GetPosition(canvas1) - mousepos;
+                draggedimage.SetValue(Canvas.LeftProperty, offset.X);
+                draggedimage.SetValue(Canvas.TopProperty, offset.Y);
+                Console.WriteLine(String.Format("{0}",offset.X));             
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog savefiledialog1 = new SaveFileDialog();
+            //savefiledialog1.
+            savefiledialog1.DefaultExt = ".png";
+            bool? userClickedOK = savefiledialog1.ShowDialog();
+
+            
+
+            if (userClickedOK == true)
+            {
+                string path = savefiledialog1.FileName;
+               // canvas1.Background.Opacity = 0.0;
+                RenderTargetBitmap RTB_Image = new RenderTargetBitmap((int)canvas1.ActualWidth, (int)canvas1.ActualHeight, 96d, 96d, PixelFormats.Pbgra32);
+                RTB_Image.Render(canvas1);
+
+                var stm = System.IO.File.Create(path);
+
+                BitmapEncoder pngEncoder = new PngBitmapEncoder();
+                pngEncoder.Frames.Add(BitmapFrame.Create(RTB_Image));
+                pngEncoder.Save(stm);
+                stm.Close();
+
+                XmlWriterSettings XWSettings = new XmlWriterSettings();
+                XWSettings.Indent = true;
+
+                int offset = path.LastIndexOf("\\");
+                path = path.Substring(0, path.LastIndexOf("."));
+                string name = path.Substring(offset + 1, path.Length - offset - 1);
+
+
+                using (XmlWriter XMLWriter = XmlWriter.Create(path + ".xml", XWSettings))
+                {
+                    XMLWriter.WriteStartDocument();
+                    XMLWriter.WriteStartElement("SprSheet");
+
+                    XMLWriter.WriteAttributeString("CWidth", canvas1.ActualWidth.ToString());
+                    XMLWriter.WriteAttributeString("CHeight", canvas1.ActualHeight.ToString());
+                    XMLWriter.WriteAttributeString("Image", name);
+                    int i = 0;
+                    foreach (Image img in canvas1.Children)
+                    {
+                        XMLWriter.WriteStartElement("Image");
+                        XMLWriter.WriteAttributeString("Name", name);//img.Source.ToString().Substring(img.Source.ToString().LastIndexOf("/") + 1, img.Source.ToString().Length - img.Source.ToString().LastIndexOf("/") - System.IO.Path.GetExtension(img.Source.ToString()).Length - 1));
+                        XMLWriter.WriteAttributeString("X", img.GetValue(Canvas.LeftProperty).ToString());
+                        XMLWriter.WriteAttributeString("Y", img.GetValue(Canvas.TopProperty).ToString());
+                        XMLWriter.WriteAttributeString("Width", img.Width.ToString());
+                        XMLWriter.WriteAttributeString("Height", img.Height.ToString());
+                        XMLWriter.WriteAttributeString("Id", i.ToString());
+                        XMLWriter.WriteEndElement();
+                        i++;
+
+                    }
+
+                    XMLWriter.WriteEndDocument();
+                } 
+            }
         }
     }
 }
